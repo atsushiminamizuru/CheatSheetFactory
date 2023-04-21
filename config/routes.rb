429d8ca52e500851b8bf1ac6afd4e02fc60933f2
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-04-21 13:54:37 +0000

    ユーザー新規登録エラー時リロードパス修正とエラーメッセージ表示修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+  end
   resources :sheets
   resources :users, only:[:show, :edit, :update, :destroy]
   
