4fc5aad99144a968c34eb88b8010266b8c8fd758
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-04-25 09:34:46 +0000

    特定ユーザーのお気に入り一覧の変数名修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -17,6 +17,6 @@ class FavoritesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     user_favorites_sheets = @user.favorites.includes(sheet: :user).order('favorites.created_at DESC')
-    @user_favoritesheets = user_favorites_sheets.map(&:sheet)
+    @user_favorite_sheets = user_favorites_sheets.map(&:sheet)
   end
 end
