bac6e37ae1723319ebb4c4e0e09407886716e457
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-05-08 16:14:50 +0000

    フォロー機能、非同期通信

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,21 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
+
   def create
     @relationship = Relationship.new(following_id: current_user.id, follower_id: params[:user_id])
     @relationship.save
-    flash[:notice] = 'It succeeded beautifully.'
-    redirect_to user_path(id: params[:user_id])
+    @user.reload
+    @relationship = Relationship.find_by(following_id: current_user.id, follower_id: @user.id)
+    render 'create.js.erb'
   end
 
   def destroy
     @relationship = Relationship.find(params[:id])
     redirect_to root_path unless current_user.id == @relationship.following_id
     @relationship.destroy
-    flash[:notice] = 'It succeeded beautifully.'
-    redirect_to user_path(id: params[:user_id])
+    @user.reload
+    @relationship = Relationship.find_by(following_id: current_user.id, follower_id: @user.id)
+    render 'destroy.js.erb'
   end
 
   def followings
@@ -22,5 +26,11 @@ class RelationshipsController < ApplicationController
   def followers
     @user = User.find(params[:user_id])
     @user_followers = @user.followers.with_attached_profile_image.order('relationships.created_at DESC').page(params[:page])
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
