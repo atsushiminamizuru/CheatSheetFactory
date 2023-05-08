b41b9b5349002603bfbe61f4724e522dcfdefc4a
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-05-08 14:11:10 +0000

    いいね機能、非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_sheet, only: [:create, :destroy]
+
   def create
     @favorite = Favorite.new(user_id: current_user.id, sheet_id: params[:sheet_id])
     @favorite.save
-    flash[:notice] = 'It succeeded beautifully.'
-    redirect_to sheet_path(id: params[:sheet_id])
+    @sheet.reload
+    render 'create.js.erb'
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     redirect_to root_path unless current_user.id == @favorite.user_id
     @favorite.destroy
-    flash[:notice] = 'It succeeded beautifully.'
-    redirect_to sheet_path(id: params[:sheet_id])
+    @sheet.reload
+    render 'destroy.js.erb'
   end
 
   def index
@@ -22,5 +24,11 @@ class FavoritesController < ApplicationController
                                  .where(favorites: { user_id: @user.id })
                                  .order('favorites.created_at DESC')
                                  .page(params[:page])
+  end
+
+  private
+
+  def set_sheet
+    @sheet = Sheet.find(params[:sheet_id])
   end
 end
